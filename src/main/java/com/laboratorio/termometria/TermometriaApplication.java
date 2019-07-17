c98c06cf87e5e5c052b86d0506d819dde291897d
Mauricio Pereyra <mpereyra@neginet.com>
2019-07-17 16:54:06 -0300

    First Commit

---
+++ src/main/java/com/laboratorio/termometria/TermometriaApplication.java
@@ -0,0 +1,13 @@
+package com.laboratorio.termometria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermometriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TermometriaApplication.class, args);
+    }
+
+}
